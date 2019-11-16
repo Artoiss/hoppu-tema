0b19730667fc9451b31680a1b5357c35ea05412e
Artoiss <aarttu@windowslive.com>
2019-11-16 21:02:48 +0200

    Router

---
+++ client/src/Main.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Link,
+  Redirect, withRouter} from 'react-router-dom'
+import App from './App'
+import Store from './Store'
+import {Button, Logo} from './components/index'
+
+const Main = () => {
+  return (
+
+      <div>
+        <Router>
+          <div>
+            <div>
+
+            <Link to="/sell">
+            <Button color={'primary'} variant={'contained'}>
+                Sell</Button>
+            </Link>
+
+            <Link to="/store">
+            <Button color={'primary'} variant={'contained'}>
+                Store</Button>
+            </Link>
+            </div>
+            <Route exact path="/sell" render={() =><App/> }/>
+            <Route exact path="/store" render={() =><Store/> }/>
+          </div>
+        </Router>
+      </div>
+    )
+  }
+
+
+
+export default Main;
