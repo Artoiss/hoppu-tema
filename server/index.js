1768c49354936d8943137968b20ede47eeac2de0
Arseni Leskinen <arseni.leskinen@fi.abb.com>
2019-11-16 19:48:17 +0200

    mega commit

--- server/index.js
+++ server/index.js
@@ -1,23 +1,79 @@
-const express = require('express')
-//const axios = require('axios')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const bodyParser = require("body-parser");
+
+const config = require("./config");
 //app.use(express.static('build'))
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/api/test', (req, res) => {
-    const test_json = {"hello_string": "Hello World!"}
-    res.json(test_json)
-})
+// const getProductById = axios.get(
+//   `https://${API_KEY}:${PASSWORD}${API_URL}/products/${product_id}.json`
+// );
 
-//app.get('api/order/:id', (req, res) => {
-    
-//})
+app.get("/api/order/:id", (req, response) => {
+  const id = req.params.id;
+  getOrder(id)
+    .then(res => {
+      const orderData = parseOrderData(res.data);
+      response.json(orderData);
+    })
+    .catch(err => response.status(500).send({ error: err }));
+});
 
-const PORT = 3001
+app.get("/api/product/:id", (req, response) => {
+  const id = req.params.id;
+  getProduct(id)
+    .then(res => {
+      console.log(res.data);
+      const orderData = parseOrderData(res.data);
+      response.json(orderData);
+    })
+    .catch(err => response.status(500).send({ error: err }));
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-	console.log(`server is running on port ${PORT}`)
-})
+  console.log(`server is running on port ${PORT} `);
+});
+const getOrder = id =>
+  axios.get(
+    `https://${config.API_KEY}:${config.PASSWORD}${config.API_URL}/orders/${id}.json`
+  );
 
+const getProduct = id =>
+  axios.get(
+    `https://${config.API_KEY}:${config.PASSWORD}${config.API_URL}/products/${id}.json`
+  );
 
-//const getOrder = axios.get(`https://${APIKEY}:${PASS}@veitsiapu.myshopify.com/admin/api/2019-10/orders/1866048143437.json`)
+const parseOrderData = data => {
+  const order = data.order;
+  console.log(order);
+  const name = order.shipping_address.first_name;
+  console.log("@@@", name);
+  const product_id = order.line_items.map(item => item.product_id);
+  console.log(product_id);
+  const title = order.line_items.map(item => item.title);
+  console.log(title);
+  return {
+    first_name: name,
+    product_id: product_id,
+    title: title
+  };
+};
+
+const parseProductData = data => {
+  const product = data.order;
+  console.log(order);
+  const name = order.shipping_address.first_name;
+  console.log("@@@", name);
+  const product_id = order.line_items.map(item => item.product_id);
+  console.log(product_id);
+  const title = order.line_items.map(item => item.title);
+  console.log(title);
+  return {
+    first_name: name,
+    product_id: product_id,
+    title: title
+  };
+};
