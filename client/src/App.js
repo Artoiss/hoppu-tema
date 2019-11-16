8b3d9095e59449b2b73cdc431ae024e1833f47fb
lpeltol <lassi.peltola@solenovo.fi>
2019-11-16 10:19:37 +0000

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hoppu-App
+        </p>
+        <button>Scan QR</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
