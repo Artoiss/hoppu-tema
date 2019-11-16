692a69f1f9167e625b5ff9b236dc90b90ca2c20f
Artoiss <aarttu@windowslive.com>
2019-11-16 14:24:45 +0200

    Updates

---
+++ precache-manifest.e8c5aa8ae2168e78ad174fa886bed6fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35362d07b56fee88bbb1b85b37c7ff2a",
+    "url": "/hoppu-tema/index.html"
+  },
+  {
+    "revision": "4bb4d0899d2d3cc7f6ba",
+    "url": "/hoppu-tema/static/css/main.9006df9e.chunk.css"
+  },
+  {
+    "revision": "a3bb7943570dffa2c7b5",
+    "url": "/hoppu-tema/static/js/2.60d05b18.chunk.js"
+  },
+  {
+    "revision": "4bb4d0899d2d3cc7f6ba",
+    "url": "/hoppu-tema/static/js/main.6274cff1.chunk.js"
+  },
+  {
+    "revision": "f8f36a3605e50464f289",
+    "url": "/hoppu-tema/static/js/runtime-main.0c28b2d5.js"
+  }
+]);
