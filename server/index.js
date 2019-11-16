d74f19ef5d6ae7ca9a9da33cc7458f8b11e8b662
Arseni Leskinen <arseni.leskinen@fi.abb.com>
2019-11-16 23:37:48 +0200

    order skulaa, product 1 tuote kerrallaan

--- server/index.js
+++ server/index.js
@@ -30,17 +30,6 @@ app.get("/api/product/:id", (req, response) => {
     .catch(err => response.status(500).send({ error: err }));
 });
 
-// app.get("/api/product_image/:id", (req, response) => {
-//   const id = req.params.id;
-//   // const image_id = req.query.imageId;
-//   getImage(id, image_id)
-//     .then(res => {
-//       const imageData = parseImageData(res.data);
-//       response.json(imageData);
-//     })
-//     .catch(err => response.status(500).send({ error: err }));
-// });
-
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT} `);
@@ -54,31 +43,14 @@ const getProduct = id =>
   axios.get(
     `https://${config.API_KEY}:${config.PASSWORD}${config.API_URL}/products/${id}.json`
   );
-//
-// const getImage = id =>
-//   axios.get(
-//     `https://${config.API_KEY}:${config.PASSWORD}${config.API_URL}/products/${id}/images.json`
-//   );
 
-// const getImageAndProduct = id => {
-//   return axios.all([
-//     axios.get(
-//       `https://${config.API_KEY}:${config.PASSWORD}${config.API_URL}/products/${id}.json`
-//     ),
-//     axios.get(
-//       `https://${config.API_KEY}:${config.PASSWORD}${config.API_URL}/products/${id}/images.json`
-//     )
-//   ]);
-// };
 const parseOrderData = data => {
   const order = data.order;
   const orderArray = order.line_items.map(item => {
-    const color = item.variant_title.split(" ")[2];
     const orderObject = {
       product_id: item.product_id,
       title: item.title,
-      total_price: item.price,
-      color: color
+      total_price: item.price
     };
     return orderObject;
   });
@@ -89,19 +61,14 @@ const parseOrderData = data => {
 
 const parseProductData = data => {
   const product = data.product;
-  const image_id = product.variants[0].image_id;
   const size = product.variants[0].option1;
+  const price = product.variants[0].price;
   const image_url = product.image.src;
+  const body = product.body_html;
   return {
+    size,
+    price,
     image_url,
-    size
+    body
   };
 };
-
-// const parseImageData = data => {
-//   const image = data.image;
-//   const image_url = image.src;
-//   return {
-//     image_url
-//   };
-// };
