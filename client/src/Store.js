e23593bc00eecff0d5db5d99d2c51725df1baec7
Artoiss <aarttu@windowslive.com>
2019-11-17 09:46:55 +0200

    more items

--- client/src/Store.js
+++ client/src/Store.js
@@ -13,13 +13,27 @@ class Store extends React.Component {
         <div className={"cards-container"}>
           <Card
             image={
-              "https://www.esportspedia.com/streamers/images/6/6f/ScarraNew.png"
+              "https://cdn.shopify.com/s/files/1/0108/6324/7424/products/descente-stock-ski-pant-2020-orange-1_2000x.jpg"
             }
             name={"Pants"}
             size={"XL"}
             price={"60€"}
             desc={"New red skiing pants, never used."}
           />
+          <Card image={"https://cdn.shopify.com/s/files/1/0217/3274/products/pau3053_106_h_940x.jpg?v=1543863314"}
+                name={"Leather shoes,"}
+                size={"42"}
+                price={"15€"}
+                desc={"New unused leather shoes"}/>
+            <Card
+                image={
+                    "https://cdn.shopify.com/s/files/1/0260/7815/0733/products/hat_360x.jpg?v=1573936316"
+                  }
+                name={"Fedora hat"}
+                size={"One size"}
+                price={"30€"}
+                desc={"Used fedora hat."}
+                />
         </div>
       </div>
     );
